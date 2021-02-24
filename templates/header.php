9d88739ba2010e65e51d26355a7a1cdc737f72af
nicholsonz <77360596+nicholsonz@users.noreply.github.com>
2021-02-24 11:22:24 -0600

    Add files via upload

---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Radio Contact Log</title>
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<div class="header">
+	<a href="index.php"><h1>--- Radio Contact Log ---</h1></a>
+</div>
